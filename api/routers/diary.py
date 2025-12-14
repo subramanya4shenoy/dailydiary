a1b19baba7a12b441a223c422e898d4a97aaa71d
subramanya4shenoy <subramanya4shenoy@gmail.com>
2025-12-14 19:39:21 +0530

    diary

--- api/routers/diary.py
+++ api/routers/diary.py
@@ -5,45 +5,52 @@ from sqlalchemy.orm import Session
 
 from auth.auth import get_current_user
 from schemas.diary import Page
-from services.diary import create_diary, read_diary_page
+from services.diary import post_diary_page, delete_diary_page, get_diary_page, put_diary_page, get_all_diary_pages
+from typing import List
 router = APIRouter()
 
+
+
 @router.get("/diary/{diary_page}")
-def get_diary_page(db: Session = Depends(get_db), current_user = Depends(get_current_user)):
-    """
-    Read a diary page from the database
-    Args:
-        diary_page: str
-    Returns:
-        diary_page: Diary
-    """
-    page = read_diary_page(diary_page, db, current_user)
+def fetch_diary_page(diary_page: str, db: Session = Depends(get_db), current_user = Depends(get_current_user)):
+    """ Get specific diary page by id """
+    page = get_diary_page(diary_page, db, current_user)
     if page is None:
         raise HTTPException(status_code=HTTP_404_NOT_FOUND, detail="Page not found")
     return { "status": "ok", "requested": page}
 
+
+
 @router.post("/diary")
-def post_diary_page(page: Page, 
+def create_diary_page(page: Page, 
                 db: Session = Depends(get_db),
                 current_user = Depends(get_current_user)):
-    """
-    Add a new diary page to the database    
-    Args:
-        page: Page
-        db: Session
-        current_user: User
-    Returns:
-        new_diary_page: Diary
-    """
-    new_diary_page = create_diary(page, db, current_user)
+    """ Create a new diary page """
+    new_diary_page = post_diary_page(page, db, current_user)
     if not new_diary_page:
         raise HTTPException(status_code=status.HTTP_400_BAD_Request, detail="Failed to create diary page")
     return { "status": "ok", "details": new_diary_page}
 
+
+
 @router.put("/diary/{diary_page}")
-def update_diary_page(diary_page):
-    return { "status": "ok", "requested": diary_page}
+def update_diary_page(diary_page: str, page: Page, db: Session = Depends(get_db), current_user = Depends(get_current_user)): 
+    """ Update a specific diary page by id """
+    page = put_diary_page(diary_page, page, db, current_user)
+    if page is None:
+        raise HTTPException(status_code=HTTP_404_NOT_FOUND, detail="Page not found")
+    return { "status": "ok", "requested": page}
+
+
 
 @router.delete("/diary/{diary_page}")
-def remove_diary_page(diary_page):
-    return { "status": "ok", "requested": diary_page}
+def remove_diary_page(diary_page: str, db: Session = Depends(get_db), current_user = Depends(get_current_user)):
+    page = delete_diary_page(diary_page, db, current_user)
+    if page is None:
+        raise HTTPException(status_code=HTTP_404_NOT_FOUND, detail="Page not found")
+    return { "status": "ok", "requested": page}
+
+@router.get("/diary", response_model=List[Page])
+def fetch_all_diary_pages(db: Session = Depends(get_db), current_user = Depends(get_current_user)):
+    pages = get_all_diary_pages(db, current_user)
+    return pages or []
--- api/services/diary.py
+++ api/services/diary.py
@@ -4,14 +4,20 @@ from models.user import User
 from models.diary import Diary
 from datetime import datetime
 
-def create_diary(page: Page, db: Session, current_user: User):
+def post_diary_page(page: Page, db: Session, current_user: User):
+    diary_id = str(datetime.now().strftime("%Y%m%d"))
+    old_page = db.query(Diary).filter(Diary.diary_id == diary_id, Diary.user_id == current_user.id).first()
+    if old_page is not None:
+        put_diary_page(diary_id, page, db, current_user)
+        return old_page
+    
     new_diary_page = Diary(
         title=page.title,
         body=page.body,
         user_id=current_user.id,
         created_at=datetime.now(),
         updated_at=datetime.now(),
-        diary_id=str(datetime.now().strftime("%Y%m%d")),
+        diary_id=diary_id,
         tags= []
     )
     db.add(new_diary_page)
@@ -20,6 +26,33 @@ def create_diary(page: Page, db: Session, current_user: User):
     return new_diary_page
 
 
-def read_diary_page(diary_id: str, db: Session):
-    page = db.query(Diary).filter(Diary.diary_id == diary_id).first()
+def get_diary_page(diary_id: str, db: Session, current_user: User):
+    page = db.query(Diary).filter(Diary.diary_id == diary_id, Diary.user_id == current_user.id).first()
     return page
+
+def put_diary_page(diary_id: str, page: Page, db: Session, current_user: User):
+    old_page = db.query(Diary).filter(Diary.diary_id == diary_id, Diary.user_id == current_user.id).first()
+    print("page =>", old_page)
+    if old_page is None:
+        return None
+    old_page.title = page.title
+    old_page.body = page.body
+    old_page.updated_at = datetime.now()
+    old_page.diary_id = diary_id
+    db.commit()
+    db.refresh(old_page)
+    return old_page
+
+def delete_diary_page(diary_id: str, db: Session, current_user: User):
+    old_page = db.query(Diary).filter(Diary.diary_id == diary_id, Diary.user_id == current_user.id).first()
+    if old_page is None:
+        return None
+    db.delete(old_page)
+    db.commit()
+    return old_page
+
+def get_all_diary_pages(db: Session, current_user: User):
+    pages = db.query(Diary).filter(Diary.user_id == current_user.id).all()
+    if pages is None:
+        return []
+    return [Page(title=page.title, body=page.body, diary_id=page.diary_id) for page in pages]
